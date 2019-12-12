e97743b707e5166af151246fbf4ffbe45e1675df
CJ Gilroy <c.gilroy9@gmail.com>
2019-12-12 10:56:40 -0700

    Get and set piece position in rows array

--- board.rb
+++ board.rb
@@ -2,4 +2,14 @@ class Board
     def initialize
         @rows = Array.new(8) { [] }
     end
+
+    def [](pos)
+        row, col = pos
+        @rows[row][col]
+    end
+
+    def []=(pos,piece)
+        row, col = pos
+        @rows[row][col] = piece
+    end
 end
