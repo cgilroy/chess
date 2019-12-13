865bdded16c16bbe5198d48110e3f08d9ed0c5cc
CJ Gilroy <c.gilroy9@gmail.com>
2019-12-13 14:54:00 -0700

    Add populate board method to initialize board

--- board.rb
+++ board.rb
@@ -1,6 +1,9 @@
+require_relative 'piece'
+require 'byebug'
 class Board
     def initialize
-        @rows = Array.new(8) { [] }
+        @rows = Array.new(8) { Array.new(8) { nil } }
+        populate_board
     end
 
     def [](pos)
@@ -12,4 +15,18 @@ class Board
         row, col = pos
         @rows[row][col] = piece
     end
+
+    def populate_board
+        # debugger
+        [0,1,6,7].each do |row_idx|
+            row = @rows[row_idx]
+            row.each_with_index do |_,col_idx|
+                self[[row_idx,col_idx]] = Piece.new('blue',self,[row_idx,col_idx])
+            end
+        end
+    end
+
 end
+
+x = Board.new
+puts x
