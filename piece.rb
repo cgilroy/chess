f2c90d9b35844454b8fbfdb0096ec489d49d320c
CJ Gilroy <c.gilroy9@gmail.com>
2019-12-21 13:10:46 -0700

    Add #move_diffs to King class

--- piece.rb
+++ piece.rb
@@ -71,8 +71,13 @@ class Queen < Piece
 end
 
 class King < Piece
+    include Stepable
     def initialize(color,board,pos)
         super(color,board,pos)
+    end
+
+    def move_diffs
+        [[1,1],[1,-1],[-1,1],[-1,-1],[1,0],[0,1],[-1,0],[0,-1]]
     end
 end
 
