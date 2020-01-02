a2dd68d7e71465fedd80ea9a833c42bd822176c6
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2020-01-02 06:11:21 -0700

    Add cursor class

--- display.rb
+++ display.rb
@@ -1,9 +1,12 @@
 require_relative 'board'
+require_relative 'cursor'
+require 'colorize'
 
 class Display
     attr_accessor :board
 
     def initialize(board)
         @board = board
+        @cursor = Cursor.new([0,0])
     end
 end
